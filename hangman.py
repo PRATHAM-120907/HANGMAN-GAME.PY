da266eca65c61820fb61a2dbe9a96ac2079ee690
PRATHAM RAJ CHOHLA <prathamchohla@gmail.com>
2025-06-04 01:07:13 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -61,7 +61,7 @@ stages = [
 lives = 6
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(chosen_word)  
+ 
 
 placeholder = "_" * len(chosen_word)
 print(placeholder)
